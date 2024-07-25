52180b1e691be4258fcc6d4a1e00b1c86eb6e068
Luan Sena <luan.sena1507@gmail.com>
2024-07-24 21:19:05 -0300

    BoardException e programação defensiva

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -6,6 +6,9 @@ public class Board {
     private Piece[][] pieces;
 
     public Board(int rows, int columns) {
+        if (rows < 1 || columns < 1) {
+            throw new BoardException("Erro ao criar o tabuleiro: deve haver pelo menos 1 linha e 1 coluna");
+        }
         this.rows = rows;
         this.columns = columns;
         pieces = new Piece[rows][columns];
@@ -15,28 +18,44 @@ public class Board {
         return rows;
     }
 
-    public void setRows(int rows) {
-        this.rows = rows;
-    }
-
     public int getColumns() {
         return columns;
     }
 
-    public void setColumns(int columns) {
-        this.columns = columns;
+    public Piece piece(int row, int column) {
+        if (!positionExits(row, column)) {
+            throw new BoardException("Posição não encontrada no tabuleiro.");
+        }
+        return pieces[row][column];
     }
 
-    public Piece piece (int rows, int columns){
-        return pieces[rows][columns];
-    }
-
-    public Piece pieces(Position position) {
+    public Piece piece(Position position) {
+        if (!positionExits(position)) {
+            throw new BoardException("Posição não encontrada no tabuleiro.");
+        }
         return pieces[position.getRow()][position.getColumn()];
     }
 
-    public void placePiece(Piece piece, Position position){
+    public void placePiece(Piece piece, Position position) { // Responsavel por colocar uma peça numa dada posiçao
+        if (thereIsAPiece(position)) {
+            throw new BoardException("Já existe uma peça nesta posição. " + position);
+        }
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
+    }
+
+    public boolean positionExits(int row, int column) {
+        return row >= 0 && row < this.rows && column >= 0 && column < this.columns;
+    }
+
+    public boolean positionExits(Position position) {
+        return positionExits(position.getRow(), position.getColumn());
+    }
+
+    public boolean thereIsAPiece(Position position) { //Verifica se há uma peça na posicao
+        if (!positionExits(position)) {
+            throw new BoardException("Posição não encontrada no tabuleiro.");
+        }
+        return piece(position) != null;
     }
 }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -27,9 +27,9 @@ public class ChessMatch {
         return mat;
     }
 
-    private void initialSetup() {//Responsavel por iniciar a partida de xadrez
+    private void initialSetup() { //Responsavel por iniciar a partida de xadrez
         board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
-        board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
+        board.placePiece(new King(board, Color.BLACK), new Position(2, 4));
         board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
     }
 
